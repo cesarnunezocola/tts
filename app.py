730a71708d9337a746d5e3285f64b000d500ca80
cesarnunezocola <64232227+cesarnunezocola@users.noreply.github.com>
2025-04-23 22:13:14 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, send_file
+from TTS.api import TTS
+import tempfile
+
+app = Flask(__name__)
+tts = TTS(model_name="tts_models/es/css10/vits", progress_bar=False, gpu=False)
+
+@app.route('/tts', methods=['POST'])
+def tts_api():
+    text = request.json.get("text", "")
+    if not text:
+        return {"error": "Texto vacío"}, 400
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as fp:
+        tts.tts_to_file(text=text, file_path=fp.name)
+        return send_file(fp.name, mimetype="audio/wav")
+
+@app.route("/")
+def index():
+    return app.send_static_file("index.html")
